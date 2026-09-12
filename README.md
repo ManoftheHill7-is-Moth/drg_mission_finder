475e38e545432e96b574cdc654cd6d4654bf0225
ManoftheHill7-is-Moth <moth.manofthehill@gmail.com>
2026-09-12 12:31:07 -0600

    mass update and bug fix
    -updated README
    -added daily/yearly mission data .json toggle
    -added UTC/local time zone toggle for search_results.txt
    -added date-timestamp at end of search_results.txt
    -fixed bug that would skip every valid mission match if it wasn't the last mission in the biome for loop
    -included yearly mission files ready to extract and use

--- README.md
+++ README.md
@@ -1,33 +1,29 @@
 # drg\_mission\_finder
 
-Prints the time that a specified mission will appear on the mission scanner in Deep Rock Galactic using the biome type, primary mission, season, etc.
+Prints any matching mission that will appear on the mission scanner in Deep Rock Galactic to a text file using the biome type, primary objective, secondary objective, warnings, mutators, cave complexity, cave length, or season.
 
-Big thanks to rolfosian for creating https://doublexp.net/, the website that this finder pulls its data from.
+Big thanks to rolfosian for creating https://doublexp.net/, the website that this finder pulls its daily data from.
+
+Extract 'data/drg\_missions\_2026-2029.7z' for up-to-date missions to search through.
+
 
 
 Recommendations for maximizing xp chances and finding a "white whale" (50k+ xp):
 
-Season 6 (ossium gems are worth a ton of xp, especially if the max of 7 are found)
-
-Haz 5+ (2 2 2 2)
-
-Azure Weald or Fungus Bogs (croppa spawns more frequently)
-
-Mining Expedition 400 Morkite (morkite provides xp after primary quota is completed)
-
-Not hollomite or dystrum (collect additional secondaries for a small amount of bonus xp)
-
-Max Cave and Complexity (3)
-
-Double XP and Lithophage Outbreak
-
-Pots o' Gold (x4 gold mined. Provides more xp than Dark Morkite with x1.44 morkite mined)
-
-Yuletide Event (error cubes have a 1:26 chance of spawning by breaking open presents)
-
-A lot of luck!
+* Season 6 (ossium gems are worth 420 xp each, so finding the max of 7 is ideal)
+* Haz 5+ (2 2 2 2)
+* Azure Weald or Fungus Bogs (croppa spawns more frequently)
+* Mining Expedition 400 Morkite (provides 1 xp per morkite deposited after the primary quota is completed)
+* Not hollomite or dystrum as they are always worth less than other secondaries. Glyphid eggs spawn swarmers worth 1 xp each whereas each gunk seed is worth 3 xp (any other mineral secondary only grants 3 xp for depositing above quota)
+* Max Cave and Complexity (3)
+* Double XP and Core Corruption (guaranteed core stone worth 1000 xp)
+* Pots o' Gold grants x4 gold mined (Dark Morkite only grants x1.44 morkite mined)
+* Yuletide Event (error cubes are worth 4000 xp and have a 1:26 chance of spawning by breaking open presents)
+* A lot of luck!
 
 
 
 Change the code to customize the mission finder to your liking and feel free to help improve it here. Rock and Stone!
-    -Moth
+
+-Moth
+
--- mission_finder.py
+++ mission_finder.py
@@ -2,8 +2,10 @@
 # find their repository here: https://github.com/rolfosian/drgmissions/
 
 import urllib.request, json, os
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 
+DATA_FORMAT = False # uses drg_missions_YYYY.json when False and YYYY-MM-DD.json when True
+LOCAL_TIME_ZONE = False # use UTC when False and local time zone when True
 BASE_URL = "http://doublexp.net/static/json/bulkmissions"
 REMOVE_CHAR = "'[]"
 mission = []
@@ -14,6 +16,11 @@ def print_whales(data):
             # skip invalid entries
             if not isinstance(val, dict) or "Biomes" not in val:
                 continue
+
+            if not LOCAL_TIME_ZONE:
+                time_zone = k # UTC
+            else:
+                time_zone = datetime.fromisoformat(k).astimezone(None).isoformat() # convert UTC to local time zone
                 
             for biome, missions in val["Biomes"].items():
                 if biome not in ["Azure Weald", "Fungus Bogs"]:
@@ -21,94 +28,120 @@ def print_whales(data):
 
                 for m in missions:
                     is_match = (
-                        m.get("PrimaryObjective") == "Mining Expedition" and
+                        #m.get("PrimaryObjective") == "Mining Expedition" and
                         m.get("SecondaryObjective") not in ["Hollomite", "Dystrum"] and
-                        "Lithophage Outbreak" in m.get("MissionWarnings", []) and
+                        "Core Corruption" in m.get("MissionWarnings", []) and
                         m.get("MissionMutator") == "Double XP" and
                         m.get("Complexity") == "3" and # [1-3]
                         m.get("Length") == "3" and # [1-3]
                         "s6" in m.get("included_in") # "s0" is unseasoned and 5, "s1" is 1 and 2, "s3" is 3 and 4     
                     )
                 
-                if is_match:
-                    mission.append(
-                        f"A WHITE WHALE!, {k}, {m['included_in']}, {biome}, "
-                        f"{m['PrimaryObjective']}, {m['SecondaryObjective']}, "
-                        f"Length {m['Length']}, Complexity {m['Complexity']}, "
-                        f"{m.get('MissionWarnings', [])}, {m.get('MissionMutator', [])}, "
-                        f"{m['CodeName']}"
-                    )
+                    if is_match:
+                        mission.append(
+                            f"A WHITE WHALE!, {time_zone}, {m['included_in']}, {biome}, "
+                            f"{m['PrimaryObjective']}, {m['SecondaryObjective']}, "
+                            f"Length {m['Length']}, Complexity {m['Complexity']}, "
+                            f"{m.get('MissionWarnings', [])}, {m.get('MissionMutator', [])}, "
+                            f"{m['CodeName']}"
+                        )
 
-search_days = 2 # change to set how many days into the future to look (0 is today, 1 is tomorrow, etc.)
-start_date = date.today() # datetime(2026, 1, 1)
-end_date = start_date + timedelta(days=search_days)
-current_date = start_date
-
-if search_days < 0:
-    print(f"ERROR: search_days {search_days}! It must be an integer >= 0!") 
-    exit() 
-
-print(f"Searching {search_days} day{'s' if search_days != 1 else ''} in the future for matches...")
-
-while current_date <= end_date:
-    date_str = current_date.isoformat() # YYYY-MM-DD
-    file_path = f"data/{date_str}.json"
-    url = f"{BASE_URL}/{date_str}.json"
-    data = None
-
-    # try fetching from computer
+def read_local_file(file_path):
     if os.path.exists(file_path):
-        try:
-            with open(file_path, 'r') as f:
-                data = json.load(f)
-                print(f"Loading '/{file_path}'")
-                
-        except Exception as e:
-            print(f"Corrupt local file '/{file_path}': {e}")
+            try:
+                with open(file_path, 'r') as f:
+                    data = json.load(f)
+                    print(f"Loading '/{file_path}'")
+                    
+            except Exception as e:
+                print(f"Corrupt local file '/{file_path}': {e}")
+    else:
+        data = None
 
-    # try fetching from http://doublexp.net/static/json/bulkmissions/"date_str".json
-    if data is None:
-        print(f"Missing '/{file_path}', fetching from {url}")
-        try:
-            with urllib.request.urlopen(url, timeout=10) as response:
-                raw = response.read().decode(response.headers.get_content_charset() or "utf-8")
-                data = json.loads(raw)
-            
-            # save to computer
-            os.makedirs("data", exist_ok=True) # ensure folder exists
-            with open(file_path, 'w') as f:
-                json.dump(data, f)
-                print(f"Saving '/{file_path}'")
-                
-        except Exception as e:
-            print(f"STOPPING: Could not fetch {date_str}.json. Error: {e}")
-            print(f"Failed to search days {current_date} to {end_date}")
-            break
+    return(data)
+
+if not DATA_FORMAT:
+    search_year = 2027 # change to select the year to search through
+    file_path = f"data/drg_missions_{search_year}.json"
+
+    # fetch from computer
+    data = read_local_file(file_path)
 
     if data:
         print_whales(data)
+    else:
+        print(f"Missing '/{file_path}'")
+        exit()
 
-    current_date += timedelta(days=1)
+    matched = f"matched in {search_year}"
+else:
+    search_days = 2 # change to set how many days into the future to look (0 is today, 1 is tomorrow, etc.)
+    start_date = date.today()
+    end_date = start_date + timedelta(days=search_days)
+    current_date = start_date
 
-matched = f"matched from {start_date} to {current_date - timedelta(days=1)}"
+    if search_days < 0:
+        print(f"ERROR: search_days {search_days}! It must be an integer >= 0!") 
+        exit() 
+
+    print(f"Searching {search_days} day{'s' if search_days != 1 else ''} in the future for matches...")
+
+    while current_date <= end_date:
+        date_str = current_date.isoformat() # YYYY-MM-DD
+        file_path = f"data/{date_str}.json"
+        url = f"{BASE_URL}/{date_str}.json"
+
+        # try fetching from computer
+        data = read_local_file(file_path)
+
+        # try fetching from http://doublexp.net/static/json/bulkmissions/"date_str".json
+        if data is None:
+            print(f"Missing '/{file_path}', fetching from {url}")
+            try:
+                with urllib.request.urlopen(url, timeout=10) as response:
+                    raw = response.read().decode(response.headers.get_content_charset() or "utf-8")
+                    data = json.loads(raw)
+            
+                # save to computer
+                os.makedirs("data", exist_ok=True) # ensure folder exists
+                with open(file_path, 'w') as f:
+                    json.dump(data, f)
+                    print(f"Saving '/{file_path}'")
+                
+            except Exception as e:
+                print(f"STOPPING: Could not fetch {date_str}.json. Error: {e}")
+                print(f"Failed to search days {current_date} to {end_date}")
+                break
+
+        if data:
+            print_whales(data)
+
+        current_date += timedelta(days=1)
+
+    actual_days = (current_date - start_date).days
+    searched = f"({actual_days} day{'s' if actual_days != 1 else ''})"
+    matched = f"matched from {start_date} to {current_date - timedelta(days=1)} {searched}"
+
 if not mission:
     print(f"No missions {matched}")
-    print(f"No changes to 'search_results.txt'")
 else:
     mission_count = len(mission)  
     mission_message = f"{mission_count} mission{'s' if mission_count != 1 else ''} {matched}"
-    print(mission_message)
 
     # cleans matched mission text before saving
     mission_clean = []
-    table = str.maketrans('', '', REMOVE_CHAR)   
+    table = str.maketrans('', '', REMOVE_CHAR)
+    mission_clean.append(mission_message)  
     for m in mission:
-        m_clean = str(m).translate(table).replace(" ,", '').strip(", ") # removes '[] and specific commas and spaces
+        m_clean = str(m).translate(table).replace(" ,", '').replace("aB", "a B").strip(", ") # removes "'[]", specific commas and spaces, and adds a space inbetween "ApocaBlooms"
         mission_clean.append(m_clean)
         print(f"{m_clean}")
-    mission_clean.append(mission_message)
-    
-    with open("search_results.txt", 'w') as f:
+    print(mission_message)
+
+    # saves matches as text file
+    datetimestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    file_result = f"search_results_{datetimestamp}.txt"
+    with open(file_result, 'w') as f:
         f.write("\n".join(mission_clean))
-        print(f"Saved matches to 'search_results.txt'")
-print(f"The next search could save over this file! Rename it or risk it being lost!")
+        print(f"Saved match{'es' if mission_count != 1 else ''} to '{file_result}'")
+        print("Consider renaming this file to better match its contents")
